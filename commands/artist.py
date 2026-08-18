f422fa1a4312e4d5a991a2dee715c65a84e2662c
JackJugend <cried.jg@gmail.com>
2026-08-18 18:44:43 +0200

    fix

--- commands/artist.py
+++ commands/artist.py
@@ -839,7 +839,7 @@ class ArtistSortView(TimedDisableView):
             lines.append(
                 f"•  **[{variables.display_album}]({release['url']})**"
                 f" — {variables.release_date} · {variables.album_format}"
-                f" — **{score_or_missingr(variables.aoty_user_score)}**"
+                f" — **{score_or_missing(variables.aoty_user_score)}**"
             )
 
 
